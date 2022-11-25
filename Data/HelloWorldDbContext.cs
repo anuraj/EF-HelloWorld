a21f0cdcbfa198afba7e8bedd4d1d014429c1adc
Anuraj <anuraj.p@msn.com>
2022-11-25 16:51:19 +0530

    4th Step - Migration

--- Data/HelloWorldDbContext.cs
+++ Data/HelloWorldDbContext.cs
@@ -23,4 +23,6 @@ public class Product
 {
     public int Id { get; set; }
     public string? Name { get; set; }
+    // public string? Description { get; set; }
+    public bool IsActive { get; set; }
 }
