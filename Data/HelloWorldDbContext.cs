0e880cff5915b8a0a939b759b8fef43462562ace
Anuraj <anuraj.p@msn.com>
2022-11-25 16:13:18 +0530

    1st Step - Migrations

---
+++ Data/HelloWorldDbContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_HelloWorld.Data;
+
+public class HelloWorldDbContext : DbContext
+{
+    public HelloWorldDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected HelloWorldDbContext()
+    {
+    }
+    public DbSet<Product> Products { get; set; } = null!;
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     modelBuilder.Entity<Product>().HasData(new Product { Id = 1, Name = "Product 1", Description = "Description 1" });
+    //     modelBuilder.Entity<Product>().HasData(new Product { Id = 2, Name = "Product 2", Description = "Description 2" });
+    // }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+}
