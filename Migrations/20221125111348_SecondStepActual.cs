dec56e0a097e988dc4fbafbdf6a8f72e2baef543
Anuraj <anuraj.p@msn.com>
2022-11-25 16:46:05 +0530

    2nd Step migration

---
+++ Migrations/20221125111348_SecondStepActual.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFHelloWorld.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondStepActual : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Product (Name, Description) VALUES ('Product 1', 'Product 1 description')");
+            migrationBuilder.Sql("INSERT INTO Product (Name, Description) VALUES ('Product 2', 'Product 2 description')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE Product WHERE Name = 'Product 1'");
+            migrationBuilder.Sql("DELETE Product WHERE Name = 'Product 2'");
+        }
+    }
+}
